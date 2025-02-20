915fb44c9883ecda886bb7414a31818c450345f9
vignan04 <22bk1a05g8@stpetershyd.com>
2025-02-20 09:32:19 +0530

    commit

--- src/components/AddStudent.tsx
+++ src/components/AddStudent.tsx
@@ -1,10 +1,131 @@
-import React from 'react'
-import NavBar from './NavBar'
+
+//import React from "react";
+import NavBar from "./NavBar";
+import "../css/my-mar-stu.css";
+import { useState } from "react";
 
 export default function AddStudent() {
+  const [rollNo, setRollNo] = useState("");
+  const [name, setName] = useState("");
+  const [math, setMath] = useState("");
+  const [science, setScience] = useState("");
+  const [english, setEnglish] = useState("");
+  const [fee, setFee] = useState("");
+  const [course, setCourse] = useState("");
+  
+
+
+
+  const saveStu = (e: FormDataEvent) => {
+    e.preventDefault();
+    console.log("submit button clicked!!!!");
+    console.log("roll no--->" + rollNo);
+    console.log("name--->" + name);
+    console.log("maths--->" + math);
+    console.log("science--->" + science);
+    console.log("english--->" + english);
+    console.log("fee--->" + fee);
+    console.log("course--->" + course);
+
+
+  };
+  const getRollNo = (e: any) => {
+    setRollNo(e.target.value);
+  };
+  const getName = (e: any) => {
+    setName(e.target.value);
+  };
+  const getMaths = (e: any) => {
+    setMath(e.target.value);
+  };
+  const getScience = (e: any) => {
+    setScience(e.target.value);
+  };
+  const getEnglish = (e: any) => {
+    setEnglish(e.target.value);
+  };
+  const getFee = (e: any) => {
+    setFee(e.target.value);
+  };
+
+  const getCourse = (e: any) => {
+    setCourse(e.target.value);
+  };
+  
   return (
     <div>
-        <NavBar/>
-        AddStudent</div>
-  )
+      <NavBar />
+      <form onSubmit={saveStu} className="stu-add-mar">
+        <div className="form-group">
+          <label>RollNo</label>
+          <input
+            type="text"
+            onChange={getRollNo}
+            className="form-control"
+            placeholder="Enter Roll no"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Name</label>
+          <input
+            type="text"
+            onChange={getName}
+            className="form-control"
+            placeholder="Enter Name"
+          />
+        </div>
+        <div className="form-group">
+          <label>Math Marks</label>
+          <input
+            type="text"
+            onChange={getMaths}
+            className="form-control"
+            placeholder="Enter Marks "
+          />
+        </div>
+        <div className="form-group">
+          <label>Sci Marks</label>
+          <input
+            type="text"
+            onChange={getScience}
+            className="form-control"
+
+            placeholder="Enter Marks "
+          />
+        </div>
+        <div className="form-group">
+          <label>Eng Marks</label>
+          <input
+            type="text"
+            onChange={getEnglish}
+            className="form-control"
+            placeholder="Enter Marks "
+          />
+        </div>
+        <div className="form-group">
+          <label>Fee</label>
+          <input
+            type="text"
+            onChange={getFee}
+            className="form-control"
+            placeholder="Enter Fee"
+          />
+        </div>
+        <div className="form-group">
+          <label>Course</label>
+          <input
+            type="text"
+            onChange={getCourse}
+            className="form-control"
+            placeholder="Enter Course"
+          />
+        </div>
+        <br />
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
